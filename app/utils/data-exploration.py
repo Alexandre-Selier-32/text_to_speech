cd2f555cf6c1ea6118c865e0cd086386280f7f9d
RitaM <mernissi.rita@gmail.com>
2023-08-28 20:21:01 +0200

    add add_phonems_and_tokens_to_df util

--- app/utils/data-exploration.py
+++ app/utils/data-exploration.py
@@ -1,6 +1,7 @@
 import os
 import soundfile as sf
 import pandas as pd
+import json
 
 def get_data_from_directory(directory_path):
     """
@@ -49,4 +50,38 @@ def get_data_from_directory(directory_path):
 
     return pd.DataFrame(data)
 
+def add_phonems_and_tokens_to_df(df, tokenized_transcript, mapping_file):
+    """
+    Ajoute les colonnes 'phonem_tokens' et 'phonems' au DataFrame en utilisant un fichier qui contient les séquences de 
+    phonems de tokens associées à chaque transcription et un fichier de mapping de phonèmes (phoneme <=> int)
+    
+    Parameters:
+    - DataFrame initial (colonne nécessaire : sequence_id)
+    - tokenized_transcript : chemin vers le fichier contenant une séquence de phonem tokens pour chaque sequence_id.
+    - mapping_file : chemin vers le fichier JSON contenant le mapping des phonèmes en tokens.
+    
+    Returns:
+    - DataFrame initial avec les colonnes 'phonem_tokens' et 'phonems'.
+    """
+    
+    with open(mapping_file, 'r') as file:
+        phoneme_mapping = json.load(file)
+    inverted_mapping = {int(value): key for key, value in phoneme_mapping.items()}
+    
+    phonem_tokens_dict = {}
+    phonems_dict = {}
 
+    with open(tokenized_transcript, 'r') as file:
+        for line in file:
+            tokens = line.strip().split()
+            sequence_id = tokens[0]
+            token_phonem_sequence = [int(token) for token in tokens[1:]]
+            phonems = [inverted_mapping[token] for token in token_phonem_sequence]
+            
+            phonem_tokens_dict[sequence_id] = token_phonem_sequence
+            phonems_dict[sequence_id] = phonems
+
+    df['phonem_tokens'] = df['sequence_id'].map(phonem_tokens_dict)
+    df['phonems'] = df['sequence_id'].map(phonems_dict)
+
+    return df
