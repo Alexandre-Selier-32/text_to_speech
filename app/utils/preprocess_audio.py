dde6ec690d9315eb5563e4f40dfc262a3118d82c
RitaM <mernissi.rita@gmail.com>
2023-09-01 00:19:27 +0200

    add create-padding-mask methods

--- app/utils/preprocess_audio.py
+++ app/utils/preprocess_audio.py
@@ -1,4 +1,6 @@
 import numpy as np
+from app.params import MEL_SPEC_PADDING_VALUE
+import tensorflow as tf
 
 def save_y_to_npy(y, path):
     np.save(path, arr= y)
@@ -17,3 +19,19 @@ def get_padded_melspecs(melspecs_dict):
 
     return padded_melspecs_dict
 
+
+def create_melspecs_padding_mask_for(melspec_seq):
+    """
+    Crée un masque de padding pour la séquence de melspec.
+
+    Les emplacements dans la séquence de melspec où il y a du padding MEL_SPEC_PADDING_VALUE (=1)
+    auront une valeur de 1 dans le masque, les autres auront une valeur de 0.
+
+    Params:
+    - sequence de mel spectrogrammes : Un tensor de shape (batch_size, seq_len) contenant des mel spec .
+
+    Return:
+    - tf.Tensor: Un tensor de shape (batch_size, 1, 1, seq_len) représentant le masque de padding.
+    """
+    melspec_seq = tf.cast(tf.math.equal(melspec_seq, MEL_SPEC_PADDING_VALUE), tf.float32)
+    return melspec_seq[:, tf.newaxis, tf.newaxis, :]
