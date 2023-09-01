13b9f215bed925fabc2ae2cdc5bb6db9edcb2992
Alexandre Selier <alex.selier32@gmail.com>
2023-09-01 10:57:46 +0200

    Add decoder

--- app/model/Decoder.py
+++ app/model/Decoder.py
@@ -1,7 +1,24 @@
 import tensorflow as tf
 from tensorflow.keras import layers
+from app.model.EncodecLayer import EncodecLayer
 
 
 class Decoder(layers.Layer):
-    def __init__(self) -> None:
-        self
+    def __init__(self, num_layers, embedding_dim, num_heads, dff,
+                conv_kernel_size, conv_filters, rate):
+        super(Decoder, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.decoder_layers = [EncodecLayer(embedding_dim, num_heads, dff, conv_kernel_size, conv_filters, rate)
+                           for _ in range(num_layers)]
+
+        self.dropout = layers.Dropout(rate)
+
+    def call(self, input, mask):
+        decoder_output = input
+        for layer in range(self.num_layers):
+            decoder_output = self.decoder_layers[layer](decoder_output, mask)
+
+        return decoder_output
