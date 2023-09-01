3cc754567176846ad098a11b4ec0139b591485d4
DylanLamaison <dylan.lamaison@outlook.fr>
2023-09-01 17:16:59 +0200

    wave to mel padded

--- app/utils/preprocess_audio.py
+++ app/utils/preprocess_audio.py
@@ -5,14 +5,14 @@ def save_y_to_npy(y, path):
 
 def get_padded_melspecs(melspecs_dict):
     melspecs_lists = list(melspecs_dict.values())
-    
+
     max_length = max(mel.shape[1] for mel in melspecs_lists)
-    
+
     padded_mels = []
     for mel in melspecs_lists:
         padded_mel = np.pad(mel, ((0, 0), (0, max_length - mel.shape[1])), 'constant', constant_values=1)
         padded_mels.append(padded_mel)
-    
+
     padded_melspecs_dict = {key: value for key, value in zip(melspecs_dict.keys(), padded_mels)}
 
     return padded_melspecs_dict
