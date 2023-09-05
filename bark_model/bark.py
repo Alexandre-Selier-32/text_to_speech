4c133db00abf51a43458ded2849a441b8833cb9a
DylanLamaison <dylan.lamaison@outlook.fr>
2023-09-05 10:55:49 +0200

    add bark_model

---
+++ bark_model/bark.py
@@ -0,0 +1,21 @@
+!pip install git+https://github.com/huggingface/transformers.git
+
+
+from transformers import AutoProcessor, AutoModel
+
+
+processor = AutoProcessor.from_pretrained("suno/bark-small")
+model = AutoModel.from_pretrained("suno/bark-small")
+
+inputs = processor(
+    text=["Hello, my name is dylan"],
+    return_tensors="pt",
+)
+
+speech_values = model.generate(**inputs, do_sample=True)
+
+
+from IPython.display import Audio
+
+sampling_rate = model.generation_config.sample_rate
+Audio(speech_values.cpu().numpy().squeeze(), rate=sampling_rate)
