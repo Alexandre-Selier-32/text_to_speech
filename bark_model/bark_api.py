d50a4200c498494ffabe064f79651fd28f6a4382
Alexandre Selier <alex.selier32@gmail.com>
2023-09-05 16:04:38 +0200

    Add serve_wav endpoint to Bark API

--- bark_model/bark_api.py
+++ bark_model/bark_api.py
@@ -9,8 +9,25 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from transformers import AutoProcessor, AutoModel
+from contextlib import asynccontextmanager
+import scipy
+from fastapi.responses import FileResponse, StreamingResponse
+import io
+import os
+from app.params import PATH_BARK_WAV, PATH_BARK_DUMMY_WAV
 
-app = FastAPI()
+model = dict()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # loading model
+    model['processor'] = AutoProcessor.from_pretrained("suno/bark-small")
+    model['model'] = AutoModel.from_pretrained("suno/bark-small")
+    print('Pretrained Model loaded')
+    yield
+    model.clear()
+
+app = FastAPI(lifespan=lifespan)
 
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
@@ -35,37 +52,35 @@ def predict(
     Makes a single prediction
     Takes in text and returns a wav file
     """
-    processor = AutoProcessor.from_pretrained("suno/bark-small")
-    model = AutoModel.from_pretrained("suno/bark-small")
 
-    inputs = processor(
+    inputs = model['processor'](
         text=[text_to_transform],
         return_tensors="pt", )
 
-
-    speech_values = model.generate(**inputs, do_sample=True)
-
-    import scipy
-    from fastapi.responses import FileResponse
-    # sampling_rate = model.config.sample_rate
-    # scipy.io.wavfile.write("bark_out.wav", rate=sampling_rate, data=speech_values.cpu().numpy().squeeze())
-
-
-    # temp_file = NamedTemporaryFile(suffix=".wav",delete=False)
-    # sf.write(temp_file.name, speech_values, samplerate=22050)
-    scipy.io.wavfile.write("bark_out.wav", rate=22050, data=speech_values.cpu().numpy().squeeze())
-
-    wav_path = 'bark_out.wav'
-
+    speech_values = model['model'].generate(**inputs, do_sample=True)
+    wav_path = os.path.join(PATH_BARK_WAV, 'bark_out.wav')
+    scipy.io.wavfile.write(wav_path, rate=22050, data=speech_values.cpu().numpy().squeeze())
     return FileResponse(wav_path, media_type='audio/wav')
+
+
+
+
+
+
+
+@app.get("/serve_wav")
+def serve(
+    wav_name='LJ001-0010.wav',
+    ):
+    dummy_wav_path = os.path.join(PATH_BARK_DUMMY_WAV, wav_name)
+
+    dummy_wav, _ = librosa.load(dummy_wav_path, sr=22050)
+    temp_file = NamedTemporaryFile(suffix=".wav",delete=False)
+    sf.write(temp_file.name, dummy_wav, samplerate=22050)
+
+    return FileResponse(temp_file.name)
 
 
 @app.get("/")
 def root():
     return dict(status="Running")
-
-
-
-@app.get("/test")
-async def main():
-    return FileResponse(some_file_path)
